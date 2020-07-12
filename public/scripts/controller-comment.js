931876c500fcdf5586e2cad5d38628e2356af68a
Naveen Kumar <456endson@gmail.com>
2020-07-12 12:37:27 +0530

    1) The add comment toggle button's textContent is changed based on the state of form view. 2) Left-Right margins are added to comment content. 3) Navbar - The entire li can now be used to navigate instaed of exactly pointing on the text. 4) Added padding to navbar li's.

--- public/scripts/controller-comment.js
+++ public/scripts/controller-comment.js
@@ -1,4 +1,5 @@
 var btn = document.querySelector(".title button");
 btn.addEventListener("click", () => {
     document.querySelector("form").classList.toggle("hide-addComment");
+    btn.textContent = btn.textContent == "Leave a Review" ? "Collapse" : "Leave a Review";
 });
