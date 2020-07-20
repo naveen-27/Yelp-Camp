c2c28049a675245b9a6a862b42f13527509a028a
Naveen Kumar <456endson@gmail.com>
2020-07-20 15:03:22 +0530

    Owned comment edit/delete btn interactivity added

--- public/scripts/controller-comment.js
+++ public/scripts/controller-comment.js
@@ -1,5 +1,14 @@
 var btn = document.querySelector(".title button");
+var edit = document.querySelectorAll("i.edit");
+
 btn.addEventListener("click", () => {
     document.querySelector("#new-comment").classList.toggle("hide-addComment");
+    document.querySelector("#new-comment").classList.toggle("border");
     btn.textContent = btn.textContent == "Leave a Review" ? "Collapse" : "Leave a Review";
 });
+
+for (let i = 0; i < edit.length; i++) {
+    edit[i].addEventListener("click", () => {
+        document.querySelector(`form[data-cmtNo="${i}"]`).classList.toggle("hide-addComment");
+    });
+}
