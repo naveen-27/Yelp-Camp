9a85502368891452dcb329bb0a1077c6a14db4df
Naveen Kumar <456endson@gmail.com>
2020-07-20 15:01:54 +0530

    More fields are added to campground schema, fields are rendered on show page/forms(related to campgrounds)

--- models/campground.js
+++ models/campground.js
@@ -3,10 +3,19 @@ const mongoose = require("mongoose");
 const campSchema = mongoose.Schema({
     name: String,
     image: String,
+    description: String,
     cost: Number,
-    rating: Number,
+    bookingStart: String,
+    bookingClose: String,
+    contact: String,
+    location: String,
+    amenities: String,
     comments: [ { type: mongoose.Schema.Types.ObjectId, ref: 'Comment' } ],
-    owner: String,
+    rating: {
+        type: Number,
+        default: 0
+    },
+    owner: String
 });
 
 module.exports = mongoose.model("Campground", campSchema);
