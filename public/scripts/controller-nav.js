8a0772730f411b244210294d1572cc08cc580d2c
Naveen Kumar <456endson@gmail.com>
2020-07-21 11:54:41 +0530

    Fixed collapse-nav behaviour during breakpoints

--- public/scripts/controller-nav.js
+++ public/scripts/controller-nav.js
@@ -1,4 +1,12 @@
-var menu = document.querySelector(".expand")
-    menu.addEventListener("click", () => {
-    document.querySelector(".collapse-links").classList.toggle("hide-nav");
+var menu = document.querySelector(".expand"),
+    nav  = document.querySelector(".collapse-links");
+
+menu.addEventListener("click", () => {
+    nav.classList.toggle("hide-nav");
 });
+
+window.addEventListener("resize", () => {
+    if (window.innerWidth > 800 && !nav.classList.contains("hide-nav")) {
+        nav.classList.add("hide-nav");
+    }
+})
